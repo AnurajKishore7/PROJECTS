23ce0f1ac84c633bca6a78d5f735e4373896c916
Anuraj Kishore <arkishore11@gmail.com>
2024-12-10 21:01:47 +0530

    Update DbConnection.java

--- BusReservation/DbConnection.java
+++ BusReservation/DbConnection.java
@@ -3,13 +3,26 @@ package BusReservation;
 import java.sql.*;
 
 public class DbConnection {
-	
-	private static final String URL = "jdbc:mysql://localhost:3306/bus_reservation";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "password";
-	
-	public static Connection getConnection() throws SQLException {
-	    return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-	}
 
+    private static final String URL = "jdbc:mysql://localhost:3306/bus_reservation";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+
+    private static Connection connection;
+
+    // Get database connection
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        return connection;
+    }
+
+    // Close database connection
+    public static void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+            System.out.println("Database connection closed.");
+        }
+    }
 }
